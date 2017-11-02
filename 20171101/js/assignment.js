c9cbaaad94567f97f4f455a55e37901dbfa59510
jinyounghwa <timotolkie@gmail.com>
2017-11-02 13:36:31 +0900

    20171102

--- 20171101/js/assignment.js
+++ 20171101/js/assignment.js
@@ -13,3 +13,19 @@ c;
 const arr = [1,2,3,4,5];
 
 let [x,y, ...rest] = arr;
+
+//템플릿 표현식 
+const roomTempC =21.5;
+let currentTempC = 19.5;
+
+const message = `The current tmeperature is ` + `${currentTempC-roomTempC}\u00b0C different than room temperature`;
+
+const fahrenheit = `The Current temperature is ${currentTempC * 9/5 + 32}\u00b0f`;
+
+// 함수의 호출과 참조
+
+function getGreeting() {
+    return "hello world";
+}
+
+getGreeting();
