56488261fd914d8e5bd4a22c21b7547824be148b
jinyounghwa <timotolkie@gmail.com>
2017-11-15 10:17:56 +0900

    20171115

---
+++ todo/todo.js
@@ -0,0 +1,11 @@
+const Controller = class{};
+const controllers = new Map([
+  ["list_list", class extends Controller{}],
+  ["list_view", class extends Controller{}],
+  ["list_new", class extends Controller{}],
+  ["list_edit", class extends Controller{}],
+  ["list_remove", class extends Controller{}],
+  ["todo_new", class extends Controller{}],
+  ["todo_edit", class extends Controller{}],
+  ["todo_remove", class extends Controller{}]
+]);
