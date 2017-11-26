74391de5f6675fce36bd67906bc23d26dc5e7570
jinyounghwa <timotolkie@gmail.com>
2017-11-26 13:36:54 +0900

    20171126_1

--- jsalgorithms_books/linkedList.js
+++ jsalgorithms_books/linkedList.js
@@ -88,3 +88,123 @@ this.insert = function(position, element) {
         return false;
     }
 }
+
+this.toString = function(){
+
+    var current = head,
+    string = '';
+    while(current){
+        string += current.element;
+        current = current.next;
+    }
+    return string;
+};
+this.indexOf = function(element) {
+    var current = head,
+    index = -1;
+
+    while(current) {
+        if(element === current.element){
+            return index;
+        }
+        index++;
+        current = current.next
+    }
+    return -1
+}
+this.remove = function(element) {
+    var index = this.indexOf(element);
+    return this.removeAt(index);
+}
+this.isEmpty = function(){
+    return length ===0;
+}
+this.size = function(){
+    return length;
+}
+this.getHead = function(){
+    return head;
+}
+
+function DoublyLinkedList(){
+    var Node = function(element){
+        this.element = element;
+        this.next = null;
+        this.prev = null;
+    };
+    var length =0;
+    var head = null;
+    var tail = null;
+    
+    this.insert = function(position, element){
+        if(position >= 0 && position <= length){
+            var node = new Node();
+                current = head,
+                previous,
+                index =0;
+            if(position ===0) {
+                if(!head){
+                    head = node;
+                    tail = node;
+                } else{
+                    node.next = current;
+                    current.prev = node;
+                    head = node;
+                }
+            }else if (position === length){
+                current = tail;
+                current.next = node;
+                node.prev = current;
+                tail = node;
+            }else{
+                while(index++ < position) {
+                    previous = current;
+                    current = current.next;
+                }
+                node.next = current;
+                previous.next = node;
+                current.prev = node;
+                node.prev = previous;
+            }
+            length++;
+            return true;
+        }else {
+            return flase;
+        }
+    }
+    this.removeAt = function(position) {
+        if(position > -1 && position < length) {
+            var current = head,
+                previous,
+                index = 0;
+                //첫번째 원소를 삭제한다.
+            if(position ===0) {
+                head =  current.next;
+
+                //원소가 하나뿐이라면 tail을 업데이트 한다. 
+                if(length ==1) {
+                    tail = null;
+                }else{
+                    head.prev = null;
+                }
+            }else if(position === length-1){
+
+                current = tail;
+                tail = current.prev;
+                tail.next = null;
+            }else{
+                while(index++ < position){
+                    previous = current;
+                    current = current.next;
+                }
+                previous.next =  current.next;
+                current.next.prev = previous;
+            }
+            length--;
+            return current.element;
+        } else {
+            return null;
+        }
+    }
+    
+};
