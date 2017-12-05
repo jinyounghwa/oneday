9d5a6e9f5f1ad7e645c563d391073d2373603b3f
jinyounghwa <timotolkie@gmail.com>
2017-12-05 15:40:12 +0900

    20171205_2

--- jsalgorithms_books/sort.js
+++ jsalgorithms_books/sort.js
@@ -129,6 +129,25 @@ function ArrayList() {
 
         return i;
     };
+    this.binarySearch() = function(item){
+        this.quickSort();
+
+        var low =0, high = array.length-1, mid, element;
+
+        while(low <= high){
+            mid = Math.floor((low + high)/2);
+            element = array[mid];
+            if(element < item){
+                low = mid +1;
+            }else if (element >item){
+                high = mid -1;
+            }else{
+                return mid;
+            }
+
+        }
+        return -1;
+    }
 }
 
 function createNonSortedArray(size){
