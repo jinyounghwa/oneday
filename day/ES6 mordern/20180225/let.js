52e803aa372f746c903b41f8932d95a7e93ba7a2
MSDN-SPECIAL\Administrator <timotolkie@gmail.com>
2018-02-25 19:50:18 +0900

    20180225_3

---
+++ day/ES6 mordern/20180225/let.js
@@ -0,0 +1,12 @@
+function getValue(condition){
+    if(condition){
+        let value = "blue";
+        //나머지코드
+        return value;
+    }else{
+        //vlaue는 여기에 존재하지 않음
+        return null;
+    }
+    //value는 여기에 존재하지 않음
+    
+}
