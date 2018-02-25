f6f6f09db9ffdbb34c44178e751bb7895eb44d47
MSDN-SPECIAL\Administrator <timotolkie@gmail.com>
2018-02-25 19:40:00 +0900

    20180225_2

---
+++ day/ES6 mordern/20180225/test.js
@@ -0,0 +1,13 @@
+function getValue(condition){
+
+    var value;
+    if(condition){
+        value = "blue";
+        //나머지 코드
+        return value;
+    }else{
+        // value는 여기서 undefined로 존재한다.
+        return null;
+    }
+    // value는 여기서 undefined로 존재한다.
+}
