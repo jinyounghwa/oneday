dcbbde5d26cabce8c258bea6df5f816e90733934
MSDN-SPECIAL\Administrator <timotolkie@gmail.com>
2018-03-04 13:37:58 +0900

    20180304_2

---
+++ day/201803/rest.js
@@ -0,0 +1,37 @@
+function pick(object, ...keys) {
+    let result = Object.create(null);
+
+    for (let i = 0; len = key.length; i++) {
+        result[keys[i]] = object[keys[i]];
+    }
+    return result;
+}
+
+//나머지 매개변수의 제한 - 나머지 매개변수는 반드시 하나이여야 한다. 마지막위치에서 사용해야 한다 다음은에러다
+
+// function pick1 (object, ... keys, last) {
+//     let result = Object.create(null);
+
+//     for (let i = 0; len = key.length; i++) {
+//         result[keys[i]] = object[keys[i]];
+//     }
+//     return result;
+// }
+
+//setter 에는 나머지 매개변수를 사용할 수 없다.
+
+// let object = {
+//     //문법에러 
+//     set name(...value){
+//         //동작정의
+//     }
+// }
+//arguments 객체는 함수를 호출 할 때 전달한 인자는 그대로 반영하여 나머지 매개변수와 함께 잘 동작한다.
+
+function checkArgs(...args){
+    console.log(args.length);
+    console.log(arguments.length);
+    console.log(args[0], arguments[0]);
+    console.log(args[1], arguments[1]);
+}
+checkArgs("a","b");
