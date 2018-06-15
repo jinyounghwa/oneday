609adb9a7265eb6f3806b82ff3a4fd9713df69a4
MSDN-SPECIAL\Administrator <timotolkie@gmail.com>
2018-06-15 18:36:07 +0900

    20180615

---
+++ day/ES6 mordern/201806/0615.js
@@ -0,0 +1,23 @@
+//super 참조를 통한 프로토타입 접근
+let person = {
+    getGreeting(){
+        return "Hello";
+    }
+};
+
+//프로토타입은 person
+
+let friend = {
+    getGreeting () {
+        return super.getGreeting() + ",hi!!";
+    }
+};
+Object.setPrototypeOf(friend, person);
+
+//프로토타입은 friend
+
+let relative = Object.create(friend);
+
+console.log(person.getGreeting());
+console.log(friend.getGreeting());
+console.log(relative.getGreeting());
