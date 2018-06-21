99878c3bd2a9d4143664cd00540611e5efbf9afb
MSDN-SPECIAL\Administrator <timotolkie@gmail.com>
2018-06-21 17:23:01 +0900

    20180620_1

--- day/ES6 mordern/201806/0619_4.js
+++ day/ES6 mordern/201806/0619_4.js
@@ -4,7 +4,18 @@
 
 
 
+function MyObject() {
+    //..
+}
+Object.defineProperty(MyObject, Symbol.hasInstance,{
+    value:function(v){
+        return false;
+    }
+});
 
+let obj = new MyObject();
+
+console.log(obj instanceof MyObject);
 
 
 
