622ec551b59254441a0b9876f4e49f21203bbf27
MSDN-SPECIAL\Administrator <timotolkie@gmail.com>
2018-06-24 13:40:22 +0900

    20180621

--- day/ES6 mordern/201806/0621.js
+++ day/ES6 mordern/201806/0621.js
@@ -1,1 +1,27 @@
 //set & map
+
+// let set = new Set();
+
+// set.add(5);
+// set.add("5");
+
+// console.log(set.size);
+
+//요소제거하기
+
+let set = new Set();
+
+set.add(5);
+set.add("5");
+
+console.log(set.has(5));
+
+set.delete(5);
+
+console.log(set.has(5));
+console.log(set.size);
+
+set.clear();
+
+console.log(set.has("5"));
+console.log(set.size);
