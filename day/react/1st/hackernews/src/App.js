f2a27fb9bf472169af3028772c77c8978e349bcf
MSDN-SPECIAL\Administrator <timotolkie@gmail.com>
2018-07-09 21:37:02 +0900

    20180709

--- day/react/1st/hackernews/src/App.js
+++ day/react/1st/hackernews/src/App.js
@@ -20,64 +20,102 @@ const list = [
   },
 ];
 
-function isSearched(searchTerm) {
-  return function(item) {
-    //조건에 따라 true,false
-    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
-  }
-}
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
-class App extends Component{
-  onSearchChange(event){
-    this.setState({searchTerm:event.target.value});
-  }
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
-    this.state = {
-      list : list,
-      searchTerm:``,
-    };
-    this.onDisminss = this.onDisminss.bind(this);
-  }
-  onDisminss(id) {
-    const isNotId = item => item.objectID !==id;
-    const updatedList = this.state.list.filter(isNotId);
-    this.setState({list : updatedList});
-  }
-  onSearchChange() {
 
+    this.state = {
+      list,
+      searchTerm: '',
+    };
+
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
   }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
   render() {
-      const { searchTerm, list} = this.state;
-      return (
-      <div className="App">
-        <form>
-          <input type="text" 
-                 value={searchTerm}
-                 onChange={this.onSearchChange}
-          />
-        </form>
-        {list.filter(isSearched(this.state.searchTerm)).map(item =>
-            <div key={item.objectID}>
-            <div>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-                <button
-                  onClick={()=>this.onDisminss(item.objectID)}
-                  type="button"
-                >dissmiss</button>
-              </span>
-            </div>
-            </div>
-        )}
+    const { searchTerm, list } = this.state;
+    return (
+      <div className="page">
+        <div className="interactions">
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          >
+            Search
+          </Search>
+        </div>
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
       </div>
-    )
+    );
   }
 }
+
+const Search = ({ value, onChange, children }) =>
+  <form>
+    {children} <input
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+  </form>
+
+const Table = ({ list, pattern, onDismiss }) =>
+  <div className="table">
+    {list.filter(isSearched(pattern)).map(item =>
+      <div key={item.objectID} className="table-row">
+        <span style={{ width: '40%' }}>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span style={{ width: '30%' }}>
+          {item.author}
+        </span>
+        <span style={{ width: '10%' }}>
+          {item.num_comments}
+        </span>
+        <span style={{ width: '10%' }}>
+          {item.points}
+        </span>
+        <span style={{ width: '10%' }}>
+          <Button
+            onClick={() => onDismiss(item.objectID)}
+            className="button-inline"
+          >
+            Dismiss
+          </Button>
+        </span>
+      </div>
+    )}
+  </div>
+
+const Button = ({
+  onClick,
+  className = '',
+  children,
+}) =>
+  <button
+    onClick={onClick}
+    className={className}
+    type="button"
+  >
+    {children}
+  </button>
 
 export default App;
