5fce9721e2856c65fb59b622cf1422c297dc84af
MSDN-SPECIAL\Administrator <timotolkie@gmail.com>
2018-10-17 23:12:03 +0900

    20181017

---
+++ day/react_tech/sample_1/sample-project/src/MyComponent.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+class MyComponent extends Component {
+    static defaultProps = {
+        name : '기본 이름'
+      }
+    static PropTypes = {
+        name : PropTypes.string,
+        age : PropTypes.number.isRequired
+    }
+    render() {
+        return(
+            <div>
+                <p>안녕하세요 제 이름은 {this.props.name} 입니다.</p>
+                <p>저는 {this.props.age}살 입니다.</p>
+            </div>
+        )
+    }
+}
+
+export default MyComponent;
