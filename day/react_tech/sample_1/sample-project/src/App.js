e7fa9f63fceda000f76fb2094487dbe0ebe3eb52
MSDN-SPECIAL\Administrator <timotolkie@gmail.com>
2018-10-18 22:57:21 +0900

    20181018

--- day/react_tech/sample_1/sample-project/src/App.js
+++ day/react_tech/sample_1/sample-project/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import MyComponent from './MyComponent';
 class App extends Component {
-
+  
   render() {
     return (
       <MyComponent name="React" age={4}/>
--- day/react_tech/sample_1/sample-project/src/MyComponent.js
+++ day/react_tech/sample_1/sample-project/src/MyComponent.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 class MyComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            number:0
+        }
+    }
     static defaultProps = {
         name : '기본 이름'
       }
@@ -13,6 +19,12 @@ class MyComponent extends Component {
             <div>
                 <p>안녕하세요 제 이름은 {this.props.name} 입니다.</p>
                 <p>저는 {this.props.age}살 입니다.</p>
+                <p>숫자: {this.state.number}</p>
+                <button onClick={()=> {
+                    this.setState({
+                        number : this.state.number + 1
+                    })
+                }}>더하기</button>
             </div>
         )
     }
