0a427361e5799ef48a9709148c9715d824043e2f
MSDN-SPECIAL\Administrator <timotolkie@gmail.com>
2018-10-23 22:33:06 +0900

    20181023

--- day/react_tech/sample_1/sample-project/src/App.js
+++ day/react_tech/sample_1/sample-project/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import EventPractice from './EventPractice';
+import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
 class App extends Component {
   
   render() {
     return (
-      <EventPractice/>
+      <ScrollBox />
     );
   }
 }
