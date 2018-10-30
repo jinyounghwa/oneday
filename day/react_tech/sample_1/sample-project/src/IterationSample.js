26aa9fecf7355c0c5ff4530cdb39ad6d13154ebc
MSDN-SPECIAL\Administrator <timotolkie@gmail.com>
2018-10-30 23:02:47 +0900

    20181030_2

---
+++ day/react_tech/sample_1/sample-project/src/IterationSample.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+
+class IterationSample extends Component {
+    state = {
+       names :  ['눈사람', '얼음', '눈', '바람'],
+       name : ''
+    };
+    handleChange = (e) => {
+        this.setState({
+            name : e.target.value
+        });
+    }
+    handleInsert = () =>{
+        //초기화
+        this.setState({
+            names : this.state.names.concat(this.state.name),
+            name : ''
+        });
+    }
+    render(){
+        const nameList = this.state.names.map(
+            (name, index) => (<li key={index}>{name}</li>)
+        );
+        return(
+            <div>
+                <input 
+                onChange={this.handleChange}
+                value={this.state.name}
+                />
+                <button onClick={this.handleInsert}>추가</button>
+            <ul>
+                {nameList}
+            </ul>
+            </div>
+        )
+    }
+}
+
+export default IterationSample;
