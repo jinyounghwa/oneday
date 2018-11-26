16e77cbdc72806c4af07877194308a5ca79598da
MSDN-SPECIAL\Administrator <timotolkie@gmail.com>
2018-11-26 22:20:07 +0900

    20181126

--- day/react_tech/sample_1/sample-project/src/index.js
+++ day/react_tech/sample_1/sample-project/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import App from'./containers/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
