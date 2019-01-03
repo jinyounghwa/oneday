cf0f74d0183fb2fa35b29db1cbdbefc88ea4720b
MSDN-SPECIAL\Administrator <timotolkie@gmail.com>
2019-01-03 14:53:59 +0900

    20190103_1

--- day/react_listen_todo/todo-list/src/modules/input.js
+++ day/react_listen_todo/todo-list/src/modules/input.js
@@ -1,15 +1,17 @@
-import { Map } from ' immutable';
-import { handleCtions, createActions} from 'redux-actions';
+import { Map } from 'immutable';
+import { handleActions, createAction } from 'redux-actions';
+
 
 const SET_INPUT = 'input/SET_INPUT';
+
+export const setInput = createAction(SET_INPUT);
+
 const initialState = Map({
-  value : ''
-})
+  value: ''
+});
 
-
-export const setInput = createActions(SET_INPUT);
-export default handleCtions({
-  [SET_INPUT] : (state, action) => {
-    return state.set('value', action.playload)
+export default handleActions({
+  [SET_INPUT]: (state, action) => {
+    return state.set('value', action.payload)
   }
 }, initialState);
--- day/react_listen_todo/todo-list/src/modules/todos.js
+++ day/react_listen_todo/todo-list/src/modules/todos.js
@@ -1,13 +1,13 @@
-import {Map, List} from 'immutable';
-import {handleActions, createActions} from 'redux-actions';
+import { Map, List } from 'immutable';
+import { handleActions, createAction } from 'redux-actions';
 
 const INSERT = 'todos/INSERT';
 const TOGGLE = 'todos/TOGGLE';
 const REMOVE = 'todos/REMOVE';
 
-export const insert = createActions(INSERT);
-export const toggle = createActions(TOGGLE);
-export const remove = createActions(REMOVE);
+export const insert = createAction(INSERT);
+export const toggle = createAction(TOGGLE);
+export const remove = createAction(REMOVE);
 
 const initialState = List([
   Map({
@@ -24,7 +24,11 @@ const initialState = List([
 
 export default handleActions({
   [INSERT]: (state, action) => {
-    const {id, text, done} = action.payload;
+    /* payload 안에 있는 id, text, done에 대한 레퍼런스를 만들어줍니다.
+    레퍼런스를 만들지 않고, 바로 push(Map(action.payload))를 해도 되지만,
+    나중에 이 코드를 보게 됐을 때, 
+    이 액션이 어떤 데이터를 처리하는지 쉽게 보기 위해서 하는 작업입니다. */
+    const { id, text, done } = action.payload;
 
     return state.push(Map({
       id,
@@ -33,12 +37,25 @@ export default handleActions({
     }));
   },
   [TOGGLE]: (state, action) => {
-    const {payload:index} = action;
+    const { payload: id } = action;
+    // = const id = action.payload;
+    /* 비구조화 할당을 통하여 id라는 레퍼런스에 action.payload란 값을 넣습니다.
+    이 작업이 필수는 아니지만, 나중에 이 코드를 보게 되었을 때 여기서의 payload가
+    어떤 값을 의미하는지 이해하기 쉬워집니다. */
 
-    return state.updateIn([index, 'done'], done=> !done)
+    // 전달받은 id 를 가지고 index 를 조회합니다.
+    const index = state.findIndex(todo => todo.get('id') === id);
+
+    // updateIn을 통해 현재 값을 참조하여 반대값으로 설정합니다.
+    return state.updateIn([index, 'done'], done => !done);
+    /* updateIn을 사용하지 않는다면 다음과 같이 작성할 수도 있습니다.
+    return state.setIn([index, 'done'], !state.getIn([0, index]));
+    어떤 코드가 더 편해 보이나요? 둘 중에 여러분이 맘에 드는 코드로 작성하면 됩니다.
+    */
   },
-  [REMOVE]:(state, action) =>{
-    const {payload:index} = action;
+  [REMOVE]: (state, action) => {
+    const { payload: id } = action;
+    const index = state.findIndex(todo => todo.get('id') === id);
     return state.delete(index);
   }
-}, initialState)
+}, initialState);
