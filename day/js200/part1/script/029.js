5c069da457b172c94f146a32b12fd6f83ddc0443
MSDN-SPECIAL\Administrator <timotolkie@gmail.com>
2019-05-02 00:54:45 +0900

    20190502

---
+++ day/js200/part1/script/029.js
@@ -0,0 +1,31 @@
+var family = {
+    'adrress' : 'Seoul',
+    members : {},
+    addFamily : function(age, name, role) {
+        this.members[role] = {
+            age : age,
+            name : name
+        };
+    },
+    getHeadcount : function() {
+        return Object.keys(this.members).length;
+    }
+};
+family.addFamily(30, 'chole', 'aunt');
+family.addFamily(3, 'lyn', 'niece');
+family.addFamily(10, 'dangdangi', 'dog');
+
+var printMenbers = function() {
+    var members = family.members;
+    for (role in members) {
+        console.log(`role =>` + role + ` , name =>` + members[role].name +` , age =>` + members[role].age);
+    }
+}
+printMenbers();
+
+var members = family.members;
+members['nephew'] = {age: 3, name:'hyun'};
+members.nice = {age:5, name:'lyn'};
+delete members.aunt;
+delete members['dog'];
+printMenbers();
