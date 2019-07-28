34a06fd9e042ab8fe2e72b7dea7475c967e16295
MSDN-SPECIAL\Administrator <timotolkie@gmail.com>
2019-07-28 23:20:35 +0900

    20190728

--- day/sample_fxjs/test/js/02.js
+++ day/sample_fxjs/test/js/02.js
@@ -11,3 +11,12 @@ function f1(limit, list) {
 }
 
 f1(3,[1,2,3,4,5,6,7,8,9,10]);
+
+function f2(limit, list) {
+    let acc = 0;
+    for (const a of L.take(limit,L.map(a => a * a, L.filter(a => a % 2 , list)))){
+        acc += a;
+    }
+    console.log(acc);
+}
+f2(3,[1,2,3,4,5,6,7,8,9,10]);
